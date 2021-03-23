02a59d32017c956cc849466be5c96a42c369547e
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-03-22 21:27:13 -0600

    chore: install plugins for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,57 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['switch-case', 'no-secrets'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:node/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:array-func/all',
+    'plugin:eslint-comments/recommended',
+    'plugin:optimize-regex/recommended',
+    'plugin:promise/recommended',
+    'plugin:switch-case/recommended',
+    'plugin:security/recommended',
+    'prettier',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'dist/**'],
+  ignorePatterns: ['.eslintrc.js', 'dist/**', 'coverage/**'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'no-secrets/no-secrets': 'error',
+    'unicorn/prevent-abbreviations': ['error', { checkFilenames: false }],
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      {
+        ignores: ['modules'],
+      },
+    ],
+    'node/no-unpublished-import': [
+      'error',
+      {
+        allowModules: ['supertest', '@nestjs/testing'],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.spec.ts', '**/*.e2e-spec.ts'],
+      extends: [
+        'plugin:jest/recommended',
+        'plugin:jest/style',
+        'plugin:jest-formatting/strict',
+      ],
+    },
+  ],
+  settings: {
+    node: {
+      tryExtensions: ['.js', '.json', '.ts'],
+    },
   },
 };
