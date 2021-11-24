47ead5861d6b0ec1b8e413a6e2552f96b3f73719
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-11-23 23:14:16 -0600

    feat: use validation pipe globally

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 
