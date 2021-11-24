dcd98daad4118b965354f7620524f04bd3edb4bb
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-11-24 00:50:12 -0600

    feat(auth): register a new user

---
+++ src/auth/controllers/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { RegisterUser } from '@/auth/dto/register-user.dto';
+import type { User } from '@/auth/entities/user.entity';
+import { AuthenticationService } from '@/auth/services/authentication.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authenticationService: AuthenticationService) {}
+
+  @Post('/register')
+  register(@Body() newUser: RegisterUser): Promise<User> {
+    return this.authenticationService.register(newUser);
+  }
+}
