9e06025d5fa13706a08d114428a2aae451837b43
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-04-26 11:26:01 -0600

    feat(auth): get current user from JWT
    Install passport and its dependencies
    Implement JWT strategy and guard
    Add decorator to get the current user
    Validate JWT's payload checking the DB

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthController } from '@/auth/controllers/auth.controller';
 import { User } from '@/auth/entities/user.entity';
 import { AuthenticationService } from '@/auth/services/authentication.service';
+import { JwtStrategy } from '@/auth/strategies/jwt.strategy';
 import { IsAlreadyRegisterConstraint } from '@/auth/validators/is-already-register.validator';
 import { ValidateCredentialConstraint } from '@/auth/validators/validate-credential.validator';
 
@@ -17,11 +19,13 @@ import { ValidateCredentialConstraint } from '@/auth/validators/validate-credent
         expiresIn: '30d',
       },
     }),
+    PassportModule,
   ],
   providers: [
     AuthenticationService,
     IsAlreadyRegisterConstraint,
     ValidateCredentialConstraint,
+    JwtStrategy,
   ],
   controllers: [AuthController],
 })
--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -2,31 +2,43 @@ import {
   Body,
   ClassSerializerInterceptor,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 
+import { CurrentUser } from '@/auth/decorators/auth.decorator';
 import { LoginUser } from '@/auth/dto/login-user.dto';
 import { RegisterUser } from '@/auth/dto/register-user.dto';
 import type { User } from '@/auth/entities/user.entity';
+import { JWTAuthGuard } from '@/auth/guards/jwt-auth.guard';
 import { TokenInterceptor } from '@/auth/interceptors/token.interceptor';
 import { AuthenticationService } from '@/auth/services/authentication.service';
 
 @Controller('auth')
-@UseInterceptors(ClassSerializerInterceptor, TokenInterceptor)
+@UseInterceptors(ClassSerializerInterceptor)
 export class AuthController {
   constructor(private readonly authenticationService: AuthenticationService) {}
 
   @Post('/register')
+  @UseInterceptors(TokenInterceptor)
   register(@Body() newUser: RegisterUser): Promise<User> {
     return this.authenticationService.register(newUser);
   }
 
   @Post('/login')
   @HttpCode(HttpStatus.OK)
+  @UseInterceptors(TokenInterceptor)
   login(@Body() credentials: LoginUser): Promise<User> {
     return this.authenticationService.login(credentials);
+  }
+
+  @Get('/me')
+  @UseGuards(JWTAuthGuard)
+  currentUser(@CurrentUser() user: User): User {
+    return user;
   }
 }
