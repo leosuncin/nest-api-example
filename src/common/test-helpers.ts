88283d23d34d812748705e8a733cfdbc6c5a0a72
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-04-29 13:13:51 -0600

    ci(github): remove warnings

--- src/common/test-helpers.ts
+++ src/common/test-helpers.ts
@@ -22,6 +22,7 @@ import {
   Resolver,
 } from 'typeorm-fixtures-cli';
 
+// eslint-disable-next-line security/detect-unsafe-regex
 export const uuidRegex = /[\da-f]{8}(?:-[\da-f]{4}){3}-[\da-f]{12}/;
 
 export const isoDateRegex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/;
