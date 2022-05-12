8ddfb37caf25095c7a6782e3efa544bc4aa27f16
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-05-12 00:57:24 -0600

    test(blog): assert that can create one comment

---
+++ test/blog/comment.e2e-spec.ts
@@ -0,0 +1,92 @@
+import { HttpStatus } from '@nestjs/common';
+import { e2e, request, spec } from 'pactum';
+
+import { createCommentFixture } from '@/blog/fixtures/comment.fixture';
+import { isoDateRegex, uuidRegex } from '@/common/test-helpers';
+
+const unauthorizedError = {
+  message: 'Unauthorized',
+  statusCode: HttpStatus.UNAUTHORIZED,
+};
+const credentials = {
+  username: 'john-doe',
+  password: 'Th€Pa$$w0rd!',
+};
+
+describe('CommentController (e2e)', () => {
+  const testCase = e2e('Comment CRUD');
+  const articleId = 'a832e632-0335-4191-8469-4d849bbb72be';
+  let tokenCookie: string;
+
+  beforeAll(() => {
+    request.setBaseUrl('http://localhost:3000');
+  });
+
+  beforeEach(async () => {
+    tokenCookie = await spec()
+      .post('/auth/login')
+      .withBody(credentials)
+      .returns(({ res }) => res.headers['set-cookie'])
+      .toss();
+  });
+
+  afterAll(async () => {
+    await testCase.cleanup();
+  });
+
+  it('add a new comment to an article', async () => {
+    const data = await createCommentFixture().execute();
+
+    await testCase
+      .step('Create comment')
+      .spec()
+      .post('/articles/{articleId}/comments')
+      .withPathParams('articleId', articleId)
+      .withHeaders('Cookie', tokenCookie)
+      .withJson(data)
+      .expectStatus(HttpStatus.CREATED)
+      .expectJsonLike({
+        article: uuidRegex,
+        author: 'typeof $V === "object"',
+        body: data.body,
+        createdAt: isoDateRegex,
+        id: uuidRegex,
+        updatedAt: isoDateRegex,
+      })
+      .stores('comment', '.')
+      .toss();
+  });
+
+  it('validate the creation of a new comment', async () => {
+    const data = {};
+
+    await spec()
+      .post('/articles/00000000-0000-0000-0000-000000000000/comments')
+      .withHeaders('Cookie', tokenCookie)
+      .withJson(data)
+      .expectStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+      .expectJson({
+        error: 'Unprocessable Entity',
+        message: [
+          'body should not be null or undefined',
+          'body should not be empty',
+          'body must be a string',
+          'article with id «00000000-0000-0000-0000-000000000000» does not exist',
+        ],
+        statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      })
+      .toss();
+  });
+
+  it('require to be authenticated to add a new comment to an article', async () => {
+    const data = await createCommentFixture().execute();
+
+    await spec()
+      .post('/articles/{articleId}/comments')
+      .withPathParams('articleId', articleId)
+      .withJson(data)
+      .expectStatus(HttpStatus.UNAUTHORIZED)
+      .expectJson(unauthorizedError)
+      .toss();
+  });
+});
