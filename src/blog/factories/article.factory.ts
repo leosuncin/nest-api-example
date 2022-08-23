8097e407dc6e9039e3e1707238999612a8784126
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-08-23 15:28:57 -0600

    chore(blog): add seeder and factory for article and comment
    Register them in the data source

---
+++ src/blog/factories/article.factory.ts
@@ -0,0 +1,12 @@
+import * as txtgen from 'txtgen';
+import { setSeederFactory } from 'typeorm-extension';
+
+import { Article } from '@/blog/entities/article.entity';
+
+export const articleFactory = setSeederFactory(Article, (faker) =>
+  Article.fromPartial({
+    title: faker.company.catchPhrase(),
+    content: txtgen.paragraph(),
+    createdAt: faker.date.past(),
+  }),
+);
--- src/data-source.ts
+++ src/data-source.ts
@@ -6,6 +6,10 @@ import { userFactory } from '@/auth/factories/user.factory';
 import { UserSeeder } from '@/auth/seeders/user.seeder';
 import { Article } from '@/blog/entities/article.entity';
 import { Comment } from '@/blog/entities/comment.entity';
+import { articleFactory } from '@/blog/factories/article.factory';
+import { commentFactory } from '@/blog/factories/comment.factory';
+import { ArticleSeeder } from '@/blog/seeders/article.seeder';
+import { CommentSeeder } from '@/blog/seeders/comment.seeder';
 import { CreateUser1637703183543 } from '@/migrations/1637703183543-create-user';
 import { CreateArticleComment1651517018946 } from '@/migrations/1651517018946-create-article-comment';
 
@@ -15,8 +19,8 @@ export const dataSourceOptions: DataSourceOptions & SeederOptions = {
   synchronize: false,
   entities: [User, Article, Comment],
   migrations: [CreateUser1637703183543, CreateArticleComment1651517018946],
-  factories: [userFactory],
-  seeds: [UserSeeder],
+  factories: [userFactory, articleFactory, commentFactory],
+  seeds: [UserSeeder, ArticleSeeder, CommentSeeder],
 };
 
 export const dataSource = new DataSource(dataSourceOptions);
