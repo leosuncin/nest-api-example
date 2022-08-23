40a382b00f0ffbd110e65bbd808668ccb5406757
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-08-23 12:10:03 -0600

    chore(auth): add seeder and factory for user
    Register them in the data source

---
+++ src/auth/factories/user.factory.ts
@@ -0,0 +1,15 @@
+import normalizeEmail from 'normalize-email';
+import { setSeederFactory } from 'typeorm-extension';
+
+import { User } from '@/auth/entities/user.entity';
+import { credentials } from '@/common/test-helpers';
+
+export const userFactory = setSeederFactory(User, (faker) =>
+  User.fromPartial({
+    bio: faker.hacker.phrase(),
+    email: normalizeEmail(faker.internet.email()),
+    image: faker.internet.avatar(),
+    password: credentials.password,
+    username: faker.internet.userName().toLowerCase(),
+  }),
+);
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,22 @@
 import { type DataSourceOptions, DataSource } from 'typeorm';
+import type { SeederOptions } from 'typeorm-extension';
 
 import { User } from '@/auth/entities/user.entity';
+import { userFactory } from '@/auth/factories/user.factory';
+import { UserSeeder } from '@/auth/seeders/user.seeder';
 import { Article } from '@/blog/entities/article.entity';
 import { Comment } from '@/blog/entities/comment.entity';
 import { CreateUser1637703183543 } from '@/migrations/1637703183543-create-user';
 import { CreateArticleComment1651517018946 } from '@/migrations/1651517018946-create-article-comment';
 
-export const dataSourceOptions: DataSourceOptions = {
+export const dataSourceOptions: DataSourceOptions & SeederOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   synchronize: false,
   entities: [User, Article, Comment],
   migrations: [CreateUser1637703183543, CreateArticleComment1651517018946],
+  factories: [userFactory],
+  seeds: [UserSeeder],
 };
 
 export const dataSource = new DataSource(dataSourceOptions);
