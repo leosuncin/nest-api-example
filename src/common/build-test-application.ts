54ad2d055113bf92645e83bce1411e7134d8f805
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-09-04 13:06:42 -0600

    chore: split database config

--- src/common/build-test-application.ts
+++ src/common/build-test-application.ts
@@ -12,8 +12,18 @@ import { useContainer } from 'class-validator';
 import cookieParser from 'cookie-parser';
 import type { DataSource } from 'typeorm';
 
+import { User } from '@/auth/entities/user.entity';
+import { userFactory } from '@/auth/factories/user.factory';
+import { UserSeeder } from '@/auth/seeders/user.seeder';
+import { Article } from '@/blog/entities/article.entity';
+import { Comment } from '@/blog/entities/comment.entity';
+import { articleFactory } from '@/blog/factories/article.factory';
+import { commentFactory } from '@/blog/factories/comment.factory';
+import { ArticleSeeder } from '@/blog/seeders/article.seeder';
+import { CommentSeeder } from '@/blog/seeders/comment.seeder';
 import { database } from '@/common/database';
-import { dataSourceOptions } from '@/data-source';
+import { CreateUser1637703183543 } from '@/migrations/1637703183543-create-user';
+import { CreateArticleComment1651517018946 } from '@/migrations/1651517018946-create-article-comment';
 
 export async function buildTestApplication(
   ...modules: Array<
@@ -23,10 +33,15 @@ export async function buildTestApplication(
   const module = await Test.createTestingModule({
     imports: [
       TypeOrmModule.forRootAsync({
-        // @ts-expect-error for testing purpose only
         useFactory: () => ({
-          ...dataSourceOptions,
-          url: undefined,
+          type: 'postgres',
+          entities: [User, Article, Comment],
+          migrations: [
+            CreateUser1637703183543,
+            CreateArticleComment1651517018946,
+          ],
+          factories: [userFactory, articleFactory, commentFactory],
+          seeds: [UserSeeder, ArticleSeeder, CommentSeeder],
           migrationsRun: true,
           autoLoadEntities: true,
         }),
