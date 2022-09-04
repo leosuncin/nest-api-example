d80fcadff5333a936b0ac1f7143dda67fc774a05
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-09-04 12:23:47 -0600

    test(blog): refactor factories

---
+++ src/blog/factories/create-article.factory.ts
@@ -0,0 +1,11 @@
+import { FactoryBuilder } from 'factory.io';
+import { paragraph, sentence } from 'txtgen';
+
+import { CreateArticle } from '@/blog/dto/create-article.dto';
+
+export const createArticleFactory = FactoryBuilder.of(CreateArticle)
+  .props({
+    content: paragraph,
+    title: sentence,
+  })
+  .build();
---
+++ src/blog/factories/update-article.factory.ts
@@ -0,0 +1,13 @@
+import { faker } from '@faker-js/faker';
+import { FactoryBuilder } from 'factory.io';
+import { paragraph, sentence } from 'txtgen';
+
+import { UpdateArticle } from '@/blog/dto/update-article.dto';
+
+export const updateArticleFactory = FactoryBuilder.of(UpdateArticle)
+  .props({
+    ...(faker.datatype.boolean()
+      ? { content: paragraph }
+      : { title: sentence }),
+  })
+  .build();
