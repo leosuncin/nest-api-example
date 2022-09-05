2d7dd11a88805545d903af7420e2eaacb58bba90
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-09-05 12:40:52 -0600

    feat: config typeorm in a single file

--- src/common/build-test-application.ts
+++ src/common/build-test-application.ts
@@ -6,24 +6,16 @@ import {
   HttpStatus,
   ValidationPipe,
 } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { useContainer } from 'class-validator';
 import cookieParser from 'cookie-parser';
 import type { DataSource } from 'typeorm';
 
-import { User } from '~auth/entities/user.entity';
-import { userFactory } from '~auth/factories/user.factory';
-import { UserSeeder } from '~auth/seeders/user.seeder';
-import { Article } from '~blog/entities/article.entity';
-import { Comment } from '~blog/entities/comment.entity';
-import { articleFactory } from '~blog/factories/article.factory';
-import { commentFactory } from '~blog/factories/comment.factory';
-import { ArticleSeeder } from '~blog/seeders/article.seeder';
-import { CommentSeeder } from '~blog/seeders/comment.seeder';
 import { database } from '~common/database';
-import { CreateUser1637703183543 } from '~migrations/1637703183543-create-user';
-import { CreateArticleComment1651517018946 } from '~migrations/1651517018946-create-article-comment';
+import { configuration } from '~config/configuration';
+import { dataSource } from '~config/data-source';
 
 export async function buildTestApplication(
   ...modules: Array<
@@ -32,20 +24,18 @@ export async function buildTestApplication(
 ): Promise<INestApplication> {
   const module = await Test.createTestingModule({
     imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+        expandVariables: true,
+        load: [configuration],
+      }),
       TypeOrmModule.forRootAsync({
-        useFactory: () => ({
-          type: 'postgres',
-          entities: [User, Article, Comment],
-          migrations: [
-            CreateUser1637703183543,
-            CreateArticleComment1651517018946,
-          ],
-          factories: [userFactory, articleFactory, commentFactory],
-          seeds: [UserSeeder, ArticleSeeder, CommentSeeder],
-          migrationsRun: true,
-          autoLoadEntities: true,
-        }),
-        dataSourceFactory: (options): Promise<DataSource> =>
+        imports: [ConfigModule.forFeature(dataSource)],
+        inject: [ConfigService],
+        useFactory(config: ConfigService) {
+          return config.getOrThrow('data-source');
+        },
+        dataSourceFactory: (options) =>
           database.adapters.createTypeormDataSource(
             options,
           ) as Promise<DataSource>,
@@ -54,6 +44,7 @@ export async function buildTestApplication(
     ],
   }).compile();
   const app = module.createNestApplication();
+  const config = app.get(ConfigService);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -62,7 +53,7 @@ export async function buildTestApplication(
       errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
     }),
   );
-  app.use(cookieParser(process.env['SECRET']));
+  app.use(cookieParser(config.get('secret', 's€cr3to')));
   useContainer(module, { fallbackOnErrors: true });
 
   return app.init();
---
+++ src/config/data-source.ts
@@ -0,0 +1,80 @@
+import { registerAs } from '@nestjs/config';
+import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import invariant from 'tiny-invariant';
+import { type DataSourceOptions, DataSource } from 'typeorm';
+import type { SeederOptions } from 'typeorm-extension';
+
+import { User } from '~auth/entities/user.entity';
+import { userFactory } from '~auth/factories/user.factory';
+import { UserSeeder } from '~auth/seeders/user.seeder';
+import { Article } from '~blog/entities/article.entity';
+import { Comment } from '~blog/entities/comment.entity';
+import { articleFactory } from '~blog/factories/article.factory';
+import { commentFactory } from '~blog/factories/comment.factory';
+import { ArticleSeeder } from '~blog/seeders/article.seeder';
+import { CommentSeeder } from '~blog/seeders/comment.seeder';
+import { CreateUser1637703183543 } from '~migrations/1637703183543-create-user';
+import { CreateArticleComment1651517018946 } from '~migrations/1651517018946-create-article-comment';
+
+// needed by TypeORM CLI
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    // eslint-disable-next-line unicorn/prevent-abbreviations
+    interface ProcessEnv {
+      readonly NODE_ENV: 'development' | 'production' | 'test';
+      readonly DATABASE_URL: string;
+    }
+  }
+}
+
+const type: DataSourceOptions['type'] = 'postgres';
+const entities: DataSourceOptions['entities'] = [User, Article, Comment];
+const migrations: DataSourceOptions['migrations'] = [
+  CreateUser1637703183543,
+  CreateArticleComment1651517018946,
+];
+const factories: SeederOptions['factories'] = [
+  userFactory,
+  articleFactory,
+  commentFactory,
+];
+const seeds: SeederOptions['seeds'] = [
+  UserSeeder,
+  ArticleSeeder,
+  CommentSeeder,
+];
+
+const dataSourceOptions: DataSourceOptions & SeederOptions = {
+  type,
+  url: process.env.DATABASE_URL,
+  synchronize: false,
+  entities,
+  migrations,
+  factories,
+  seeds,
+};
+
+export const dataSource = registerAs('data-source', () => {
+  if (process.env.NODE_ENV === 'test') {
+    return {
+      type,
+      entities,
+      migrations,
+      factories,
+      seeds,
+      migrationsRun: true,
+    } as DataSourceOptions & SeederOptions;
+  }
+
+  invariant(process.env.DATABASE_URL, 'DATABASE_URL is missing');
+
+  return {
+    type,
+    url: process.env.DATABASE_URL,
+    synchronize: false,
+    autoLoadEntities: true,
+  } as TypeOrmModuleOptions;
+});
+
+export default new DataSource(dataSourceOptions);
