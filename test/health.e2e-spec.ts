7e44d19b115b818b8df9d9100a3d9f40b2a6c073
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-09-14 00:11:03 -0600

    test(health): check the health of the app

---
+++ test/health.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { HttpStatus } from '@nestjs/common';
+import { request, spec } from 'pactum';
+
+describe('HealthController (e2e)', () => {
+  beforeAll(() => {
+    request.setBaseUrl('http://localhost:3000');
+  });
+
+  it('/health (GET)', async () => {
+    await spec()
+      .get('/health')
+      .expectStatus(HttpStatus.OK)
+      .expectJson({
+        status: 'ok',
+        info: {
+          database: {
+            status: 'up',
+          },
+          mem_rss: {
+            status: 'up',
+          },
+          mem_heap: {
+            status: 'up',
+          },
+        },
+        error: {},
+        details: {
+          database: {
+            status: 'up',
+          },
+          mem_rss: {
+            status: 'up',
+          },
+          mem_heap: {
+            status: 'up',
+          },
+        },
+      })
+      .toss();
+  });
+});
