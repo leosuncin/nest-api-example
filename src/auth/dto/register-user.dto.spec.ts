7e936e58f36b7820d5864fdcb8e820d698b7d38c
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-09-14 18:06:45 -0600

    refactor(auth): if the email and username exists
    Move the DB logic into the AuthenticationService
    Update tests

--- src/auth/dto/register-user.dto.spec.ts
+++ src/auth/dto/register-user.dto.spec.ts
@@ -1,21 +1,18 @@
 import { faker } from '@faker-js/faker';
 import { HttpStatus } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
 import { plainToInstance } from 'class-transformer';
 import { useContainer, validate } from 'class-validator';
 import fc from 'fast-check';
 import nock, { cleanAll, enableNetConnect } from 'nock';
 import { createMock } from 'ts-auto-mock';
-import type { Repository } from 'typeorm';
 
 import { RegisterUser } from '~auth/dto/register-user.dto';
-import { User } from '~auth/entities/user.entity';
 import { registerUserFactory } from '~auth/factories/register-user.factory';
+import { register } from '~auth/fixtures/credentials';
 import { PASSWORD_HASHES } from '~auth/fixtures/password-hashes';
+import { AuthenticationService } from '~auth/services/authentication.service';
 import { IsAlreadyRegisterConstraint } from '~auth/validators/is-already-register.validator';
-
-import { register } from '../fixtures/credentials';
 
 describe('Register user validations', () => {
   beforeAll(async () => {
@@ -23,8 +20,10 @@ describe('Register user validations', () => {
       providers: [IsAlreadyRegisterConstraint],
     })
       .useMocker((token) => {
-        if (token === getRepositoryToken(User)) {
-          return createMock<Repository<User>>();
+        if (token === AuthenticationService) {
+          return createMock<AuthenticationService>({
+            userNotExistWith: jest.fn().mockResolvedValue(true),
+          });
         }
 
         return;
