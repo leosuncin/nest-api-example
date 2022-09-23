ba8d7e8e64a516797d7d01e00fbc2f4e689a37d2
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-09-23 04:48:30 +0000

    feat: install swagger module
    Enable compiler plugin

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
 import cookieParser from 'cookie-parser';
 
 import { AppModule } from '~app/app.module';
 import type { ConfigObject } from '~config/configuration';
 
+const swaggerConfig = new DocumentBuilder()
+  .setTitle('Blog API')
+  .setDescription('The Blog API description')
+  .setVersion('1.0.0')
+  .build();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigService<ConfigObject, true>>(ConfigService);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
 
   app.useGlobalPipes(new ValidationPipe(config.get('validation')));
   app.use(cookieParser(config.getOrThrow('secret')));
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  SwaggerModule.setup('/', app, document);
 
   await app.listen(config.get('port'));
 }
