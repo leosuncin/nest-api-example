7917a7847812f693290ab7059a1ce56635aaab3b
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-09-25 17:03:44 -0600

    test(health): amend the mock of HealthService

--- src/health.controller.spec.ts
+++ src/health.controller.spec.ts
@@ -1,7 +1,9 @@
 import {
+  type HealthCheckResult,
+  type HealthCheckStatus,
+  type HealthIndicatorFunction,
+  type HealthIndicatorResult,
   HealthCheckService,
-  HealthIndicatorFunction,
-  HealthIndicatorResult,
   MemoryHealthIndicator,
   TypeOrmHealthIndicator,
 } from '@nestjs/terminus';
@@ -14,6 +16,48 @@ function getStatus(key: string): HealthIndicatorResult {
   return { [key]: { status: 'up' } };
 }
 
+function reduceResults(
+  results: Array<HealthIndicatorResult>,
+): HealthIndicatorResult {
+  const summary: HealthIndicatorResult = {};
+
+  for (const result of results) {
+    Object.assign(summary, result);
+  }
+
+  return summary;
+}
+
+async function checkHealthIndicators(
+  indicators: Array<HealthIndicatorFunction>,
+): Promise<HealthCheckResult> {
+  const success: Array<HealthIndicatorResult> = [];
+  const errors: Array<HealthIndicatorResult> = [];
+  const results = await Promise.allSettled(
+    indicators.map((indicator) => indicator()),
+  );
+
+  for (const result of results) {
+    if (result.status === 'fulfilled') {
+      success.push(result.value);
+    } else {
+      errors.push(result.reason as HealthIndicatorResult);
+    }
+  }
+
+  const status = (errors.length > 0 ? 'error' : 'ok') as HealthCheckStatus;
+  const info = reduceResults(success);
+  const error = reduceResults(errors);
+  const details = reduceResults([...success, ...errors]);
+
+  return {
+    status,
+    info,
+    error,
+    details,
+  };
+}
+
 describe('HealthController', () => {
   let controller: HealthController;
 
@@ -24,11 +68,7 @@ describe('HealthController', () => {
       .useMocker((token) => {
         if (token === HealthCheckService) {
           return createMock<HealthCheckService>({
-            check: jest
-              .fn()
-              .mockImplementation((indicators: HealthIndicatorFunction[]) =>
-                Promise.all(indicators.map((indicator) => indicator())),
-              ),
+            check: jest.fn().mockImplementation(checkHealthIndicators),
           });
         }
 
@@ -57,24 +97,21 @@ describe('HealthController', () => {
   });
 
   it('should check the health', async () => {
-    await expect(controller.healthCheck()).resolves.toMatchInlineSnapshot(`
-      [
-        {
-          "database": {
-            "status": "up",
-          },
-        },
-        {
-          "mem_rss": {
-            "status": "up",
-          },
-        },
-        {
-          "mem_heap": {
-            "status": "up",
-          },
-        },
-      ]
-    `);
+    await expect(
+      controller.healthCheck(),
+    ).resolves.toMatchObject<HealthCheckResult>({
+      details: {
+        database: { status: 'up' },
+        mem_heap: { status: 'up' },
+        mem_rss: { status: 'up' },
+      },
+      error: {},
+      info: {
+        database: { status: 'up' },
+        mem_heap: { status: 'up' },
+        mem_rss: { status: 'up' },
+      },
+      status: 'ok',
+    });
   });
 });
